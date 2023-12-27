d6d9e4d3c202c6ef538431b24c836502c43e4670
Seungwoo Hong <qksn1541@gmail.com>
2023-12-27 10:45:33 +0900

    feat: .

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 use pyo3::prelude::*;
 use ulid::{Ulid};
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn test() -> PyResult<String> {
+#[pyo3(name="ulid")]
+fn get_ulid() -> PyResult<String> {
     Ok(Ulid::new().to_string())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn pkpy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(test, m)?)?;
+    m.add_function(wrap_pyfunction!(get_ulid, m)?)?;
     Ok(())
 }
