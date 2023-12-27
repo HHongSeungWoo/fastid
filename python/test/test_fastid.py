4123d6ce3843ef3fc220e2a2b8006aacc1d10d3d
Seungwoo Hong <qksn1541@gmail.com>
2023-12-27 13:26:30 +0900

    feat: add snowflake

--- python/test/test_fastid.py
+++ python/test/test_fastid.py
@@ -6,5 +6,11 @@ from ulid import microsecond
 
 def test_ulid():
     print()
-    print(timeit.timeit(lambda: microsecond.new().str, number=10000))
-    print(timeit.timeit(fastid.ulid, number=10000))
+    print("ulid-py    ", timeit.timeit(lambda: microsecond.new().str, number=10000))
+    print("fastid.ulid", timeit.timeit(lambda: fastid.ulid(), number=10000))
+
+
+def test_snowflake():
+    print()
+    print("fastid.snowflake_int", timeit.timeit(lambda: fastid.snowflake_int(), number=10000))
+    print("fastid.snowflake_str", timeit.timeit(lambda: fastid.snowflake_str(), number=10000))
