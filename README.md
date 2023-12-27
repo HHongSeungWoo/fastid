e40ea9ec0a1448de3d9ef2e602f4474438dd7625
Seungwoo Hong <qksn1541@gmail.com>
2023-12-27 14:21:12 +0900

    feat: add uuid

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Created for the easy and fast generation of various IDs
 # Supported IDs
 - ulid
 - snowflake
+- uuid v7 (If you intend to use uuid in your database, this can be a great choice)
--- fastid.pyi
+++ fastid.pyi
@@ -1,8 +1,14 @@
 def ulid() -> str:
     ...
 
+
 def snowflake_str(machine_id: int = 1, node_id: int = 1) -> str:
     ...
 
+
 def snowflake_int(machine_id: int = 1, node_id: int = 1) -> int:
+    ...
+
+
+def uuid_v7() -> str:
     ...
