0dd2302ef35bb602823eaadbf7fccfd116617823
Seungwoo Hong <qksn1541@gmail.com>
2023-12-27 10:45:04 +0900

    feat: add python test

---
+++ python/tests/test.py
@@ -0,0 +1,21 @@
+import pkpy
+from ulid import microsecond
+
+if __name__ == '__main__':
+    import time
+
+    t = time.time()
+
+    for i in range(1000):
+        a = microsecond.new().str
+        print(a)
+
+    print(time.time() - t)
+
+    t = time.time()
+
+    for i in range(1000):
+        a = pkpy.ulid()
+        print(a)
+
+    print(time.time() - t)
